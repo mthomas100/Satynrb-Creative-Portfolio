0fea58b199369ea7c074ed40f9663bb43eda4e22
mthomas100 <matthewsamuelthomas@gmail.com>
2020-12-25 15:23:43 -0500

    removed site border from layout.js

--- gatsby-SATYN/src/components/Layout.js
+++ gatsby-SATYN/src/components/Layout.js
@@ -13,7 +13,7 @@ const SiteBorderStyles = styled.div`
   /* margin-top: clamp(2rem, 10vw, 12rem); */
   /* background: white url(${stripes}); */
   /* background-size: 1500px; */
-  box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.044);
+  /* box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.044); */
   @media (max-width: 1100px) {
     padding-left: 0.5rem;
     padding-right: 0.5rem;
