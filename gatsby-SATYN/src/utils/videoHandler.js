1d17290b8cd394554d70c5ebd3d23bb4fe6216ce
mthomas100 <matthewsamuelthomas@gmail.com>
2020-12-26 14:43:35 -0500

    video validation changes

--- gatsby-SATYN/src/utils/videoHandler.js
+++ gatsby-SATYN/src/utils/videoHandler.js
@@ -7,27 +7,17 @@ function getYoutubeId(url) {
 }
 
 function getVimeoId(url) {
-  // false if not vimeo URL
-  let id = false;
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://vimeo.com/api/oembed.json?url=${url}`, false);
-  request.onload = function () {
-    if (request.status >= 200 && request.status < 400) {
-      const response = JSON.parse(request.responseText);
-      if (response.video_id) {
-        id = response.video_id;
-      }
-    }
-  };
-  request.send();
-  return id;
+  const regExp = /^.*(vimeo\.com\/)((channels\/[A-z]+\/)|(groups\/[A-z]+\/videos\/))?([0-9]+)/;
+  const match = url.match(regExp);
+
+  return match[5] || null;
 }
 
 export default function videoHandler(url) {
+  console.log(url);
   if (getYoutubeId(url)) {
     const youtubeId = getYoutubeId(url);
     const youtubeEmbedLink = `https://www.youtube.com/embed/${youtubeId}`;
-    console.log(youtubeEmbedLink);
     return youtubeEmbedLink;
   }
   if (getVimeoId(url)) {
--- sanity-SATYN/schemas/video.js
+++ sanity-SATYN/schemas/video.js
@@ -17,64 +17,15 @@ export default {
           : null;
       }
       
-      function getVimeoId(url) { //false if not vimeo URL
-        var id = false;
-        var request = new XMLHttpRequest();
-        request.open('GET', 'https://vimeo.com/api/oembed.json?url='+url , false);
-        request.onload = function() {
-          if (request.status >= 200 && request.status < 400) {
-            var response = JSON.parse(request.responseText);
-            if(response.video_id) {
-              id = response.video_id;
-            }
-          }
-        };
-        request.send();
-        return id;
+      function getVimeoId(url) {
+        const regExp = /^.*(vimeo\.com\/)((channels\/[A-z]+\/)|(groups\/[A-z]+\/videos\/))?([0-9]+)/;
+        const match = url.match(regExp);
+      
+        return match[5] || null;
       }
       
       return (getYoutubeId(input) || getVimeoId(input))
         ? true
         : 'Please enter a URL from either Youtube or Vimeo'
     })
-}
-
-
-function getYoutubeId(url) { //null if not youtube URL
-  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
-  const match = url.match(regExp);
-
-  return (match && match[2].length === 11)
-    ? match[2]
-    : null;
-}
-
-function getVimeoId(url) { //false if not vimeo URL
-  var id = false;
-  var request = new XMLHttpRequest();
-  request.open('GET', 'https://vimeo.com/api/oembed.json?url='+url , false);
-  request.onload = function() {
-    if (request.status >= 200 && request.status < 400) {
-      var response = JSON.parse(request.responseText);
-      if(response.video_id) {
-        id = response.video_id;
-      }
-    }
-  };
-  request.send();
-  return id;
-}
-
-function videoHandler(url) {
-    if (getYoutubeId(url)) {
-      const youtubeId = getYoutubeId('https://www.youtube.com/embed/-o2o91UIjPk');
-      const youtubeEmbedLink = `www.youtube.com/embed/${youtubeId}`;
-      return youtubeEmbedLink;
-    }
-    if (getVimeoId(url)) {
-      const vimeoId = getVimeoId('https://vimeo.com/184859361');
-      const vimeoEmbedLink = `https://player.vimeo.com/video/${vimeoId}`
-      return vimeoEmbedLink;
-    }
-    return "not a valid URL"
 }
