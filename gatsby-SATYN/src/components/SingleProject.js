4b366e73a974e7b95a82021c7ba83618bc6b6308
mthomas100 <matthewsamuelthomas@gmail.com>
2020-12-30 11:55:39 -0500

    took styled components out of component

--- gatsby-SATYN/src/components/SingleProject.js
+++ gatsby-SATYN/src/components/SingleProject.js
@@ -3,52 +3,53 @@ import styled from 'styled-components';
 import Img from 'gatsby-image';
 import { Link } from 'gatsby';
 
+const ProjectDetailsStyles = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 2;
+  display: none;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color: rgba(255, 255, 255, 0.2);
+  backdrop-filter: blur(4px);
+  color: rgba(0, 0, 0, 0.7);
+  padding: 0 2em;
+  transition: all 0.8s;
+  .title {
+    font-weight: 600;
+    letter-spacing: 3px;
+    padding-bottom: 0.25em;
+  }
+  .subtitle {
+    letter-spacing: 3px;
+    font-weight: 400;
+    padding-top: 0.25em;
+  }
+`;
+
+const CardStyles = styled.div`
+  position: relative;
+  a {
+    padding: 0;
+  }
+  .gatsby-image-wrapper {
+    width: 100%;
+    padding-top: 100%; /* 1:1 Aspect Ratio */
+    /* position: relative; */
+    z-index: 1;
+  }
+  &:hover ${ProjectDetailsStyles} {
+    display: flex;
+  }
+`;
+
 export default function SingleProject({ project }) {
   const myRef = useRef(null);
 
   const [width, setWidth] = useState(500);
-
-  const ProjectDetailsStyles = styled.div`
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    z-index: 2;
-    display: none;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    background-color: rgba(255, 255, 255, 0.2);
-    color: rgba(0, 0, 0, 0.7);
-    backdrop-filter: blur(4px);
-    padding: 0 2em;
-    .title {
-      /* font-size: calc(${width / 300}em); */
-      font-weight: 600;
-      letter-spacing: 3px;
-    }
-    .subtitle {
-      /* font-size: calc(${width / 450}em); */
-      letter-spacing: 3px;
-      font-weight: 400;
-    }
-  `;
-
-  const CardStyles = styled.div`
-    position: relative;
-    a {
-      padding: 0;
-    }
-    .gatsby-image-wrapper {
-      width: 100%;
-      padding-top: 100%; /* 1:1 Aspect Ratio */
-      /* position: relative; */
-      z-index: 1;
-    }
-    &:hover ${ProjectDetailsStyles} {
-      display: flex;
-    }
-  `;
 
   useEffect(() => {
     function handleResize() {
@@ -56,7 +57,9 @@ export default function SingleProject({ project }) {
       setWidth(myRef.current.clientWidth);
     }
 
-    window.addEventListener('load', handleResize);
+    if (myRef && myRef.current) {
+      window.addEventListener('load', handleResize);
+    }
     window.addEventListener('resize', handleResize);
 
     return () => {
